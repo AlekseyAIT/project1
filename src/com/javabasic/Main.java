626d666e3bb2a0cc84bea5730af2213d9286c79e
Aleksey Astakhov <astahov.2004@ukr.net>
2021-10-05 16:29:57 +0300

    Initial commit

--- src/com/javabasic/Main.java
+++ src/com/javabasic/Main.java
@@ -3,6 +3,10 @@ package com.javabasic;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! 123");
+    }
+
+    public static void someMethod() {
+
     }
 }

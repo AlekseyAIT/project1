7038d5571eaf8e7216ecf35ddd5da73bf09d9afa
Aleksey Astakhov <astahov.2004@ukr.net>
2021-10-05 16:48:36 +0300

    Added new message

--- src/com/javabasic/Main.java
+++ src/com/javabasic/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! 123456");
+        System.out.println("Hello World 123123123");
     }
 }

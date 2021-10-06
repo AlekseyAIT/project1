3985e428d488aaf4375c5555c1aa6f2945c9e77b
AlekseyAIT <91905663+AlekseyAIT@users.noreply.github.com>
2021-10-06 18:42:10 +0300

    Delete src/com/javabasic directory

--- src/com/javabasic/Main.java
+++
@@ -1,9 +0,0 @@
-package com.javabasic;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World! 123456");
-        System.out.println("Hello World 123123123");
-    }
-}
